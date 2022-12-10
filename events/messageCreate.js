a07aea9c1eed3fe5d781a9772bd3ae3946a72ace
ABDULRAHMAN NOFAL <56323614+ARMN88@users.noreply.github.com>
2022-12-10 02:43:58 +0000

    Initial commit

---
+++ events/messageCreate.js
@@ -0,0 +1,9 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.MessageCreate,
+	async execute(message) {
+    // await message.react("✅");
+    // await message.react("❌");
+	}
+};
